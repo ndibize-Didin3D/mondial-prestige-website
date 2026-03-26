9ca2668b2afabd9e8d726f70d7ef7e15264ed0fc
ndibize-Didin3D <ndibize@gmail.com>
2026-03-26 10:52:44 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // يغطي جميع ملفاتك
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
